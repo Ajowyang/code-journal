60f2ab9c4312ef77232cf3645df7d2d3398f76f5
ajowyang <andrewowyang875@gmail.com>
2024-07-30 19:11:27 +0000

    removed css styling that was unnecessary and tested functionality again

--- js/data.js
+++ js/data.js
@@ -1,8 +1,13 @@
 'use strict';
-/* exported data */
-let data = {
-  view: 'entry-form',
-  entries: [],
-  editing: null,
-  nextEntryId: 1,
-};
+let data = readData();
+function writeData() {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem('data', dataJSON);
+}
+function readData() {
+  const dataJsonString = localStorage.getItem('data');
+  if (!dataJsonString) {
+    return { view: '', entries: [], editing: null, nextEntryId: 1 };
+  }
+  return JSON.parse(dataJsonString);
+}
--- js/main.js
+++ js/main.js
@@ -1,21 +1,44 @@
 'use strict';
-/* global data */
-let $urlInput = document.querySelector('#photo-URL');
+/* import global data */
+const $urlInput = document.querySelector('#photo-URL');
 if (!$urlInput) throw new Error('.photo-URL-input query failed!');
-//querying url input
-let $previewImg = document.querySelector('.preview-img');
+// querying photo url input
+const $previewImg = document.querySelector('.preview-img');
 if (!$previewImg) throw new Error('.preview-img query failed!');
-//querying preview img
-let $titleInput = document.querySelector('#title');
+// querying preview img
+const $titleInput = document.querySelector('#title');
 if (!$titleInput) throw new Error('#title query failed!');
-let $entryForm = document.querySelector('#entry-form');
+// query title input
+const $notes = document.querySelector('#notes');
+if (!$notes) throw new Error('#notes query failed!');
+// query notes text area
+const $entryForm = document.querySelector('#entry-form');
 if (!$entryForm) throw new Error('.entryForm query failed!');
-//querying entry form
-$urlInput.addEventListener('input', function (event) {
+// querying entry form
+$urlInput.addEventListener('input', function () {
   const inputValue = $urlInput.value;
   console.log(inputValue);
   $previewImg.src = inputValue;
 });
 $entryForm.addEventListener('submit', function (event) {
   event.preventDefault();
+  //prevents the page from refreshing when the form is submitted
+  const newObj = {
+    title: $titleInput.value,
+    photoUrl: $urlInput.value,
+    notes: $notes.value,
+    entryId: data.nextEntryId,
+  };
+  //stores the form's input values in a new object
+  //assigns an `entryId` property to the new object, taken from the `nextEntryId` property of the data model.
+  data.nextEntryId++;
+  //increments the `nextEntryId` property of the data model so if another entry submitted later, it will receive diff `entryId`.
+  data.entries.unshift(newObj);
+  //adds the new object to the beginning of the data model's array of entries.
+  // console.log(newObj);
+  writeData();
+  $previewImg.setAttribute('src', '../images/placeholder-image-square.jpg');
+  //resets the preview image's `src` attribute back to the placeholder image.
+  $entryForm.reset();
+  //resets form
 });
